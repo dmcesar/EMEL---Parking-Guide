8a0c658e4de19709346d1e0c6fdd1d747ee43236
RuiRezendes <a21702572@alunos.ulht.pt>
2020-04-18 22:28:23 +0000

    CONTACTS FRAGMENT AND LAYOUT

--- app/src/main/java/com/example/projetocm_g11/view/ContactsFragment.kt
+++ app/src/main/java/com/example/projetocm_g11/view/ContactsFragment.kt
@@ -1,5 +1,7 @@
 package com.example.projetocm_g11.view
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -7,13 +9,30 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.projetocm_g11.R
+import kotlinx.android.synthetic.main.fragment_contacts.*
+import kotlinx.android.synthetic.main.fragment_contacts.view.*
 
 class ContactsFragment : Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
+
+
         val view = inflater.inflate(R.layout.fragment_contacts, container, false)
 
+        view.call_gen_info.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:211163060")
+            startActivity(intent)
+        }
+
+        view.call_vehicle_lock_info.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:217803131")
+            startActivity(intent)
+        }
         return view
+
     }
+
 }
